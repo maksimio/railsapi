64fe287a5cf031eec6bf3a56ebc0dc73eb16111f
Maksim L <maksim.lopatin.spb@gmail.com>
2023-10-10 09:32:01 +0300

    правила валидации

--- app/models/apply.rb
+++ app/models/apply.rb
@@ -1,4 +1,7 @@
 class Apply < ApplicationRecord
     belongs_to :geek
     belongs_to :job
+
+    validates :read, :invited, :geek_id, :job_id, presence: true
+    validates :geek_id, :job_id, numericality: { only_integer: true }
 end
--- app/models/job.rb
+++ app/models/job.rb
@@ -1,4 +1,8 @@
 class Job < ApplicationRecord
     belongs_to :company
     has_many :applies
+
+    validates :name, :place,  length: { minimum: 2, maximum: 42 }
+    validates :name, :place, :company_id, presence: true
+    validates :company_id, numericality: { only_integer: true }
 end
